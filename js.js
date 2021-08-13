f343fd5cc7321a766db66d7e17c46b6d85347718
BakrAli2021 <87548560+BakrAli2021@users.noreply.github.com>
2021-08-13 16:19:11 +0400

    Update js.js

--- js.js
+++ js.js
@@ -13,12 +13,62 @@ let textarea = document.querySelector('#txt'),
     voiceBtn = document.querySelector('.box .btn');
 voiceBtn.addEventListener('click', function() {
     let text = textarea.value;
-    responsiveVoice.speak(text);
-    voiceBtn.classList.add('active');
-    setTimeout(() => {
-        voiceBtn.classList.remove('active');
-    }, 3000)
+    if (text !== "") {
+        responsiveVoice.speak(text);
+        voiceBtn.classList.add('active');
+
+        setTimeout(() => {
+            voiceBtn.classList.remove('active');
+            search(text);
+        }, 3000);
+    } else {
+        alert("Speak Or Write Please");
+    }
+
+
 });
 select.addEventListener('change', function() {
     responsiveVoice.setDefaultVoice(select.value);
 });
+
+
+let btn = document.querySelector('.inputbx .icon'),
+    icon = document.querySelector('.inputbx .icon i');
+let SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+if (!SpeechRecognition) {
+    alert("NOt Supported with this broweser please change to Chrome");
+} else {
+    let recognition = new SpeechRecognition();
+    btn.addEventListener("click", () => {
+        btn.classList.add('active');
+
+        // icon.classList.toggle('fa-microphone-slash');
+        if (icon.classList.contains('fa-microphone')) {
+
+            recognition.start()
+
+        } else {
+
+            recognition.stop();
+        }
+
+    });
+    recognition.onstart = function() {
+        icon.classList.replace('fa-microphone', 'fa-microphone-slash');
+
+    }
+    recognition.onend = function() {
+        icon.classList.replace('fa-microphone-slash', 'fa-microphone');
+        btn.classList.remove('active');
+
+    }
+    recognition.addEventListener('result', function(event) {
+
+        let transcript = event.results[0][0].transcript;
+        textarea.value = transcript
+    })
+}
+
+function search(searchvalue) {
+    window.location.href = `https://www.google.com/search?q=${searchvalue}`;
+}
